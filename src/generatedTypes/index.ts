3dd04c2fbfa0f6af36e67949ffd83ccc9a73af6d
Alfetopito <alfetopito@users.noreply.github.com>
2022-07-08 16:50:00 +0100

    Adding generated types from schema
    Not really sure this should be added to be honest :shrug:

---
+++ src/generatedTypes/index.ts
@@ -0,0 +1,6 @@
+// generated file, do not edit manually
+
+export * as v0_1_0 from 'generatedTypes/v0.1.0'
+export * as v0_2_0 from 'generatedTypes/v0.2.0'
+export * as v0_3_0 from 'generatedTypes/v0.3.0'
+export * as v0_4_0 from 'generatedTypes/v0.4.0'
