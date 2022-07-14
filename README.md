ce86682ba920b34602bc0a52d646fc2b4f2b87d3
Leandro Boscariol <alfetopito@users.noreply.github.com>
2022-07-14 03:57:26 -0700

    Add schemas (#1)
    * Added gitignore
    
    * Updated package.json and yarn.lock
    
    * Added basic tsconfig
    
    * Adding schemas
    
    * Adding script to compile schemas and generate types
    
    * Adding index.ts which exports generated types
    
    * Adding generated types from schema
    
    Not really sure this should be added to be honest :shrug:
    
    * Updated README
    
    * The description of SlippageBips was set as title,
    
    making a terrible type name. No actual schema changes
    
    * Fixing old repo name on package.json
    
    * Using relative imports because cannot use the library otherwise
    
    * Moved schema to root folder to be exported in an easier path
    
    * Adjusting the schema ids
    
    * Refactor: renamed src/schema to src schemas :shrug:
    
    * Refactor: updating schema ids (again)
    
    * Refactor: introduced definitions.json subschema
    
    * Refactor: exposing generated schemas on `schemas` instead of `schema` folder
    
    * New dependency to bundle schema files
    
    * New generated types, now simpler with a single defintion.json schema
    
    * Refactor: updated compileSchemas script
    
    * Oops, clear command was not doing what I meant for generatedTypes folder
    
    * Tagging version as 0.0.1-RC.0
    
    * Do not ignore `src/schemas` folder :/
    
    * Add definitions file, which was ignored by mistake
    
    * Refactor: renamed clearBuild to clean
    
    * Refactor: renamed compileSchemas to compile
    
    * Refactor: auto formatting tsconfig
    
    * Refactor: simplified array deconstruction by removing unnecessary comma
    
    * Improved package description and readme
    
    * Updated Metadata title to make the generated type cleaner
    
    * Also renamed version title to make generated types better

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# appDataSchema
-AppData Schema definitions
+# @cowprotocol/app-data
+
+AppData schema definitions
+
+These schemas are used in the data encoded on `appData` field for CowProtocol orders.
+
+For more details, check [the docs](https://docs.cow.fi/cow-sdk/order-meta-data-appdata).
+
+## Usage
+
+Schemas are exposed as json files, where the version is the file name:
+
+```js
+// Getting the version v0.4.0
+const schema = require('@cowprotocol/app-data/schemas/v0.4.0.json')
+
+// Now you can for example run validation against a schema
+```
+
+There are also type definitions
+
+```js
+import { v0_4_0 } from '@cowprotocol/app-data'
+
+function createAppDataV0_4_0(
+  appCode: v0_4_0.AppCode, 
+  metadata: v0_4_0.Metadata
+): v0_4_0.AppDataRootSchema {
+  return {
+    version: '0.4.0',
+    appCode,
+    metadata
+  }
+}
+```
+
+## TODO:
+
+- [ ] Setup npm publishing
+- [ ] Add tests for:
+  - [ ] Consuming only the schema
+  - [ ] Importing the type definitions
---
+++ src/scripts/compile.ts
@@ -0,0 +1,56 @@
+import {compileFromFile} from 'json-schema-to-typescript'
+import $RefParser from "json-schema-ref-parser"
+import * as fs from 'fs'
+import * as path from 'path'
+
+const SCHEMAS_SRC_PATH = path.join('src', 'schemas')
+const SCHEMAS_DEST_PATH = 'schemas'
+const TYPES_DEST_PATH = path.join('src', 'generatedTypes')
+
+async function compile(): Promise<void> {
+  // Creates destinations dirs
+  console.info(`Creating '${TYPES_DEST_PATH}' and '${SCHEMAS_DEST_PATH}' dirs`)
+  await fs.promises.mkdir(TYPES_DEST_PATH, {recursive: true})
+  await fs.promises.mkdir(SCHEMAS_DEST_PATH)
+
+  // Generates out file for types/index.ts
+  const typesIndexPath = path.join(TYPES_DEST_PATH, 'index.ts')
+  console.info(`Creating ${typesIndexPath} file`)
+  const typesIndexFile = await fs.promises.open(typesIndexPath, 'w')
+  await typesIndexFile.write(`// generated file, do not edit manually\n\n`)
+
+  // Lists all schemas
+  const schemas = await fs.promises.readdir(SCHEMAS_SRC_PATH, {withFileTypes: true})
+
+  for (const schemaFileName of schemas) {
+    // Ignores folders and non version schemas
+    if (!schemaFileName.isFile() || !/^v\d+\.\d+\.\d+\.json$/.test(schemaFileName.name)) continue
+
+    // Extracts version from file name
+    const [version] = schemaFileName.name.split("\.json")
+
+    // Get schema path relative to repo root
+    const schemaPath = path.join(SCHEMAS_SRC_PATH, schemaFileName.name)
+
+    // Compiles schema files de-referencing `$ref`s
+    console.info(`Compiling bundled schema file for ${schemaPath}`)
+    const newSchemaFile = await $RefParser.bundle(schemaPath)
+    await fs.promises.writeFile(path.join(SCHEMAS_DEST_PATH, `${version}.json`), JSON.stringify(newSchemaFile))
+
+    // Compiles schema onto ts type declarations
+    console.info(`Compiling ts typings for ${schemaPath}`)
+    const tsFile = await compileFromFile(schemaPath, {cwd: SCHEMAS_SRC_PATH})
+    await fs.promises.writeFile(path.join(TYPES_DEST_PATH, `${version}.ts`), tsFile)
+
+    // Add export on types/index.ts for this version
+    console.info(`Adding ts export for ${version}`)
+    const exportName = version.replace(/\./g, '_')
+    const versionImportPath = `./${version}`
+    await typesIndexFile.write(`export * as ${exportName} from '${versionImportPath}'\n`)
+
+  }
+
+  await typesIndexFile.close()
+}
+
+compile()
