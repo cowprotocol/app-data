4ce8e07ac44ddae200b1131007e7224e0b92eea0
Alfetopito <alfetopito@users.noreply.github.com>
2022-07-15 12:27:52 +0100

    Added utils for creating the latest version of documents

---
+++ src/utils.ts
@@ -0,0 +1,32 @@
+import {latest, LATEST_APP_DATA_VERSION, LATEST_QUOTE_METADATA_VERSION, LATEST_REFERRER_METADATA_VERSION} from "./generatedTypes"
+
+// TODO: Cannot understand why this doesn't work. TS complains saying the type parameters are unknown
+// export function createQuoteMetadata(params: Omit<latest.Quote, 'version'>): latest.Quote {
+export function createQuoteMetadata(params: { slippageBips: latest.SlippageBips }): latest.Quote {
+  return {
+    ...params,
+    version: LATEST_QUOTE_METADATA_VERSION
+  }
+}
+
+// TODO: And this approach doesnt work for the same reason :shrug:
+// export function createReferrerMetadata({address}: Omit<latest.Referrer, 'version'>): latest.Referrer {
+export function createReferrerMetadata(params: { address: latest.ReferrerAddress }): latest.Referrer {
+  return {
+    ...params,
+    version: LATEST_REFERRER_METADATA_VERSION
+  }
+}
+
+// TODO: same as previous 2...
+// export function createAppDataDoc(params: Omit<AppDataDoc, 'version'>): latest.AppDataRootSchema {
+export function createAppDataDoc(params: {
+  appCode?: latest.AppCode
+  environment?: latest.Environment
+  metadata: latest.Metadata
+}): latest.AppDataRootSchema {
+  return {
+    ...params,
+    version: LATEST_APP_DATA_VERSION
+  }
+}
