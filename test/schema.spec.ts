9d2535d1d33bab19ae71ce15fd01116cc6faea18
Alfetopito <alfetopito@users.noreply.github.com>
2022-07-22 09:28:49 +0100

    Refactored schema tests

--- test/schema.spec.ts
+++ test/schema.spec.ts
@@ -1,4 +1,4 @@
-import Ajv from 'ajv'
+import Ajv, { ValidateFunction } from 'ajv'
 
 import schemaV0_1_0 from '../schemas/v0.1.0.json'
 import schemaV0_2_0 from '../schemas/v0.2.0.json'
@@ -29,62 +29,43 @@ describe('Schema v0.1.0', () => {
     metadata: {},
   }
 
-  test('Minimal valid schema', () => {
-    // given
-    // when
-    const actual = validator(BASE_DOCUMENT)
-    // then
-    expect(actual).toBeTruthy()
-  })
+  test('Minimal valid schema', _buildAssertValidFn(validator, BASE_DOCUMENT))
 
-  test('Missing required fields', () => {
-    // given
-    const doc = {}
-    // when
-    const actual = validator(doc)
-    // then
-    expect(actual).toBeFalsy()
-    expect(validator.errors).toEqual(MISSING_VERSION_ERROR)
-  })
+  test('Missing required fields', _buildAssertInvalidFn(validator, {}, MISSING_VERSION_ERROR))
 
-  test('With referrer metadata', () => {
-    // given
-    const doc = {
+  test(
+    'With referrer metadata',
+    _buildAssertValidFn(validator, {
       ...BASE_DOCUMENT,
       appCode: 'MyApp',
       metadata: {
         referrer: REFERRER_V0_1_0,
       },
-    }
-    // when
-    const actual = validator(doc)
-    // then
-    expect(actual).toBeTruthy()
-  })
+    })
+  )
 
-  test('With invalid referrer metadata', () => {
-    // given
-    const doc = {
-      ...BASE_DOCUMENT,
-      appCode: 'MyApp',
-      metadata: {
-        referrer: { address: '0xas', version: '0.1.0' },
-      },
-    }
-    // when
-    const actual = validator(doc)
-    // then
-    expect(actual).toBeFalsy()
-    expect(validator.errors).toEqual([
+  test(
+    'With invalid referrer metadata',
+    _buildAssertInvalidFn(
+      validator,
       {
-        instancePath: '/metadata/referrer/address',
-        keyword: 'pattern',
-        message: 'must match pattern "^0x[a-fA-F0-9]{40}$"',
-        params: { pattern: '^0x[a-fA-F0-9]{40}$' },
-        schemaPath: '#/properties/metadata/properties/referrer/properties/address/pattern',
+        ...BASE_DOCUMENT,
+        appCode: 'MyApp',
+        metadata: {
+          referrer: { address: '0xas', version: '0.1.0' },
+        },
       },
-    ])
-  })
+      [
+        {
+          instancePath: '/metadata/referrer/address',
+          keyword: 'pattern',
+          message: 'must match pattern "^0x[a-fA-F0-9]{40}$"',
+          params: { pattern: '^0x[a-fA-F0-9]{40}$' },
+          schemaPath: '#/properties/metadata/properties/referrer/properties/address/pattern',
+        },
+      ]
+    )
+  )
 })
 
 describe('Schema v0.2.0', () => {
@@ -96,64 +77,45 @@ describe('Schema v0.2.0', () => {
     metadata: {},
   }
 
-  test('Minimal valid schema', () => {
-    // given
-    // when
-    const actual = validator(BASE_DOCUMENT)
-    // then
-    expect(actual).toBeTruthy()
-  })
+  test('Minimal valid schema', _buildAssertValidFn(validator, BASE_DOCUMENT))
 
-  test('Missing required fields', () => {
-    // given
-    const doc = {}
-    // when
-    const actual = validator(doc)
-    // then
-    expect(actual).toBeFalsy()
-    expect(validator.errors).toEqual(MISSING_VERSION_ERROR)
-  })
+  test('Missing required fields', _buildAssertInvalidFn(validator, {}, MISSING_VERSION_ERROR))
 
-  test('With referrer v0.1.0 and quote v0.1.0 metadata', () => {
-    // given
-    const doc = {
+  test(
+    'With referrer v0.1.0 and quote v0.1.0 metadata',
+    _buildAssertValidFn(validator, {
       ...BASE_DOCUMENT,
       appCode: 'MyApp',
       metadata: {
         referrer: REFERRER_V0_1_0,
         quote: QUOTE_V0_1_0,
       },
-    }
-    // when
-    const actual = validator(doc)
-    // then
-    expect(actual).toBeTruthy()
-  })
+    })
+  )
 
-  test('With invalid quote metadata v0.1.0', () => {
-    // given
-    const doc = {
-      ...BASE_DOCUMENT,
-      appCode: 'MyApp',
-      metadata: {
-        referrer: REFERRER_V0_1_0,
-        quote: { sellAmount: 'xx12d', buyAmount: 0.13, version: '0.1.0' },
-      },
-    }
-    // when
-    const actual = validator(doc)
-    // then
-    expect(actual).toBeFalsy()
-    expect(validator.errors).toEqual([
+  test(
+    'With invalid quote metadata v0.1.0',
+    _buildAssertInvalidFn(
+      validator,
       {
-        instancePath: '/metadata/quote/sellAmount',
-        keyword: 'pattern',
-        message: 'must match pattern "^\\d+$"',
-        params: { pattern: '^\\d+$' },
-        schemaPath: '#/properties/metadata/properties/quote/properties/buyAmount/pattern',
+        ...BASE_DOCUMENT,
+        appCode: 'MyApp',
+        metadata: {
+          referrer: REFERRER_V0_1_0,
+          quote: { sellAmount: 'xx12d', buyAmount: 0.13, version: '0.1.0' },
+        },
       },
-    ])
-  })
+      [
+        {
+          instancePath: '/metadata/quote/sellAmount',
+          keyword: 'pattern',
+          message: 'must match pattern "^\\d+$"',
+          params: { pattern: '^\\d+$' },
+          schemaPath: '#/properties/metadata/properties/quote/properties/buyAmount/pattern',
+        },
+      ]
+    )
+  )
 })
 
 describe('Schema v0.3.0', () => {
@@ -165,27 +127,13 @@ describe('Schema v0.3.0', () => {
     metadata: {},
   }
 
-  test('Minimal valid schema', () => {
-    // given
-    // when
-    const actual = validator(BASE_DOCUMENT)
-    // then
-    expect(actual).toBeTruthy()
-  })
+  test('Minimal valid schema', _buildAssertValidFn(validator, BASE_DOCUMENT))
 
-  test('Missing required fields', () => {
-    // given
-    const doc = {}
-    // when
-    const actual = validator(doc)
-    // then
-    expect(actual).toBeFalsy()
-    expect(validator.errors).toEqual(MISSING_VERSION_ERROR)
-  })
+  test('Missing required fields', _buildAssertInvalidFn(validator, {}, MISSING_VERSION_ERROR))
 
-  test('With environment and full metadata (both v0.1.0)', () => {
-    // given
-    const doc = {
+  test(
+    'With environment and full metadata (both v0.1.0)',
+    _buildAssertValidFn(validator, {
       ...BASE_DOCUMENT,
       appCode: 'MyApp',
       environment: 'prod',
@@ -193,12 +141,8 @@ describe('Schema v0.3.0', () => {
         referrer: REFERRER_V0_1_0,
         quote: QUOTE_V0_1_0,
       },
-    }
-    // when
-    const actual = validator(doc)
-    // then
-    expect(actual).toBeTruthy()
-  })
+    })
+  )
 })
 
 describe('Schema v0.4.0', () => {
@@ -210,27 +154,13 @@ describe('Schema v0.4.0', () => {
     metadata: {},
   }
 
-  test('Minimal valid schema', () => {
-    // given
-    // when
-    const actual = validator(BASE_DOCUMENT)
-    // then
-    expect(actual).toBeTruthy()
-  })
+  test('Minimal valid schema', _buildAssertValidFn(validator, BASE_DOCUMENT))
 
-  test('Missing required fields', () => {
-    // given
-    const doc = {}
-    // when
-    const actual = validator(doc)
-    // then
-    expect(actual).toBeFalsy()
-    expect(validator.errors).toEqual(MISSING_VERSION_ERROR)
-  })
+  test('Missing required fields', _buildAssertInvalidFn(validator, {}, MISSING_VERSION_ERROR))
 
-  test('With quote metadata v0.2.0', () => {
-    // given
-    const doc = {
+  test(
+    'With quote metadata v0.2.0',
+    _buildAssertValidFn(validator, {
       ...BASE_DOCUMENT,
       appCode: 'MyApp',
       environment: 'prod',
@@ -238,36 +168,50 @@ describe('Schema v0.4.0', () => {
         referrer: REFERRER_V0_1_0,
         quote: QUOTE_V0_2_0,
       },
-    }
+    })
+  )
+
+  test(
+    'With invalid quote metadata v0.2.0',
+    _buildAssertInvalidFn(
+      validator,
+      {
+        ...BASE_DOCUMENT,
+        appCode: 'MyApp',
+        environment: 'prod',
+        metadata: {
+          referrer: REFERRER_V0_1_0,
+          quote: QUOTE_V0_1_0,
+        },
+      },
+      [
+        {
+          instancePath: '/metadata/quote',
+          keyword: 'required',
+          message: "must have required property 'slippageBips'",
+          params: { missingProperty: 'slippageBips' },
+          schemaPath: '#/properties/metadata/properties/quote/required',
+        },
+      ]
+    )
+  )
+})
+
+function _buildAssertValidFn(validator: ValidateFunction, doc: any) {
+  return () => {
     // when
     const actual = validator(doc)
     // then
     expect(actual).toBeTruthy()
-  })
+  }
+}
 
-  test('With invalid quote metadata v0.2.0', () => {
-    // given
-    const doc = {
-      ...BASE_DOCUMENT,
-      appCode: 'MyApp',
-      environment: 'prod',
-      metadata: {
-        referrer: REFERRER_V0_1_0,
-        quote: QUOTE_V0_1_0,
-      },
-    }
+function _buildAssertInvalidFn(validator: ValidateFunction, doc: any, errors: any) {
+  return () => {
     // when
     const actual = validator(doc)
     // then
     expect(actual).toBeFalsy()
-    expect(validator.errors).toEqual([
-      {
-        instancePath: '/metadata/quote',
-        keyword: 'required',
-        message: "must have required property 'slippageBips'",
-        params: { missingProperty: 'slippageBips' },
-        schemaPath: '#/properties/metadata/properties/quote/required',
-      },
-    ])
-  })
-})
+    expect(validator.errors).toEqual(errors)
+  }
+}
