c30456f8e4991b96ef61fc3a94581de9901ecaa5
Anxo Rodriguez <anxolin@gmail.com>
2023-07-18 12:50:15 +0100

    Make the code more self esplanatory

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -1,5 +1,10 @@
 import type { CID, MultibaseDecoder } from 'multiformats/cid'
 
+// CID uses multibase to self-describe the encoding used (See https://github.com/multiformats/multibase)
+//   - Most reference implementations (multiformats/cid or Pinata, etc) use base58btc encoding
+//   - However, the backend uses base16 encoding (See https://github.com/cowprotocol/services/blob/main/crates/app-data-hash/src/lib.rs#L64)
+const MULTIBASE_BASE16 = 'f'
+
 export async function parseCid(ipfsHash: string): Promise<CID> {
   const { CID } = await import('multiformats/cid')
 
@@ -13,13 +18,13 @@ export async function decodeCid(bytes: Uint8Array): Promise<CID> {
 }
 
 async function getDecoder<Prefix extends string>(ipfsHash: string): Promise<MultibaseDecoder<Prefix> | undefined> {
-  if (ipfsHash[0] === 'f') {
+  if (ipfsHash[0] === MULTIBASE_BASE16) {
     // Base 16 encoding
     const { base16 } = await import('multiformats/bases/base16')
     return base16
   }
 
-  // Use default decoder
+  // Use default decoder (base58btc)
   return undefined
 }
 
