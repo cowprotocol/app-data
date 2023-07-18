bb1ced2b206d7d02f95f3f1bc177d04600f2b338
Anxo Rodriguez <anxolin@gmail.com>
2023-07-18 13:34:23 +0100

    Simplify AJV instantiation

--- src/api/validateAppDataDoc.ts
+++ src/api/validateAppDataDoc.ts
@@ -4,51 +4,44 @@ import { AnyAppDataDocVersion } from '../generatedTypes'
 import { ValidationResult } from 'types'
 import { importSchema } from '../importSchema'
 
-let ajv: AjvType
-
-let _ajv: AjvType
-let ajv$: Promise<AjvType>
+let _ajvPromise: Promise<AjvType> | undefined
 
 async function getAjv(): Promise<AjvType> {
-    if (_ajv) return _ajv
+  if (!_ajvPromise) {
+    _ajvPromise = import('ajv').then(({ default: Ajv }) => new Ajv())
+  }
 
-    if (ajv$) return ajv$
-
-    return ajv$ = import('ajv').then(({default: Ajv}) => {
-        _ajv = new Ajv()
-
-        return _ajv
-    })
+  return _ajvPromise
 }
 
 export async function validateAppDataDoc(appDataDoc: AnyAppDataDocVersion): Promise<ValidationResult> {
-    const { version } = appDataDoc
+  const { version } = appDataDoc
 
-    const ajv = await getAjv()
+  const ajv = await getAjv()
 
-    let validator = ajv.getSchema(version)
+  let validator = ajv.getSchema(version)
 
-    if (!validator) {
-        let schema
-        try {
-            schema = await importSchema(version)
-        } catch (e) {
-            if (e instanceof Error) {
-                return {
-                    success: false,
-                    errors: e.message,
-                }
-            } else {
-                throw e
-            }
+  if (!validator) {
+    let schema
+    try {
+      schema = await importSchema(version)
+    } catch (e) {
+      if (e instanceof Error) {
+        return {
+          success: false,
+          errors: e.message,
         }
-
-        ajv.addSchema(schema, version)
-        validator = ajv.getSchema(version) as ValidateFunction
+      } else {
+        throw e
+      }
     }
 
-    const success = !!validator(appDataDoc)
-    const errors = validator.errors ? ajv.errorsText(validator.errors) : undefined
+    ajv.addSchema(schema, version)
+    validator = ajv.getSchema(version) as ValidateFunction
+  }
 
-    return { success, errors }
+  const success = !!validator(appDataDoc)
+  const errors = validator.errors ? ajv.errorsText(validator.errors) : undefined
+
+  return { success, errors }
 }
