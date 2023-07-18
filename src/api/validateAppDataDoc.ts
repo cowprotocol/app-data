2781a430b003cd32af300249698a554c4e229594
Anxo Rodriguez <anxolin@gmail.com>
2023-07-18 12:51:53 +0100

    Improve AJV instanciation
    Co-authored-by: Alexandr Kazachenko <shoom3301@gmail.com>

--- src/api/validateAppDataDoc.ts
+++ src/api/validateAppDataDoc.ts
@@ -6,37 +6,49 @@ import { importSchema } from '../importSchema'
 
 let ajv: AjvType
 
+let _ajv: AjvType
+let ajv$: Promise<AjvType>
+
+async function getAjv(): Promise<AjvType> {
+    if (_ajv) return _ajv
+
+    if (ajv$) return ajv$
+
+    return ajv$ = import('ajv').then(({default: Ajv}) => {
+        _ajv = new Ajv()
+
+        return _ajv
+    })
+}
+
 export async function validateAppDataDoc(appDataDoc: AnyAppDataDocVersion): Promise<ValidationResult> {
-  const { version } = appDataDoc
+    const { version } = appDataDoc
 
-  if (!ajv) {
-    const { default: Ajv } = await import('ajv')
-    ajv = new Ajv()
-  }
+    const ajv = await getAjv()
 
-  let validator = ajv.getSchema(version)
+    let validator = ajv.getSchema(version)
 
-  if (!validator) {
-    let schema
-    try {
-      schema = await importSchema(version)
-    } catch (e) {
-      if (e instanceof Error) {
-        return {
-          success: false,
-          errors: e.message,
+    if (!validator) {
+        let schema
+        try {
+            schema = await importSchema(version)
+        } catch (e) {
+            if (e instanceof Error) {
+                return {
+                    success: false,
+                    errors: e.message,
+                }
+            } else {
+                throw e
+            }
         }
-      } else {
-        throw e
-      }
+
+        ajv.addSchema(schema, version)
+        validator = ajv.getSchema(version) as ValidateFunction
     }
 
-    ajv.addSchema(schema, version)
-    validator = ajv.getSchema(version) as ValidateFunction
-  }
+    const success = !!validator(appDataDoc)
+    const errors = validator.errors ? ajv.errorsText(validator.errors) : undefined
 
-  const success = !!validator(appDataDoc)
-  const errors = validator.errors ? ajv.errorsText(validator.errors) : undefined
-
-  return { success, errors }
+    return { success, errors }
 }
