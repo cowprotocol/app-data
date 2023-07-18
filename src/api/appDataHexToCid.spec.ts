1d7289837334b178b2f57b9ace94f2d1735cbdd4
Anxo Rodriguez <anxolin@gmail.com>
2023-07-18 12:31:42 +0100

    Add missing tests

--- src/api/appDataHexToCid.spec.ts
+++ src/api/appDataHexToCid.spec.ts
@@ -1,7 +1,23 @@
-import { appDataHexToCidLegacy } from './appDataHexToCid'
-import { APP_DATA_HEX_LEGACY, CID_LEGACY } from '../mocks'
+import { appDataHexToCid, appDataHexToCidLegacy } from './appDataHexToCid'
+import { APP_DATA_HEX, APP_DATA_HEX_LEGACY, CID, CID_LEGACY } from '../mocks'
 
 describe('appDataHexToCid', () => {
+  test('Happy path', async () => {
+    // when
+    const decodedAppDataHex = await appDataHexToCid(APP_DATA_HEX)
+    // then
+    expect(decodedAppDataHex).toEqual(CID)
+  })
+
+  test('Throws with wrong hash format ', async () => {
+    // when
+    const promise = appDataHexToCid('invalidHash')
+    // then
+    await expect(promise).rejects.toThrow()
+  })
+})
+
+describe('appDataHexToCidLegacy', () => {
   test('Happy path', async () => {
     // when
     const decodedAppDataHex = await appDataHexToCidLegacy(APP_DATA_HEX_LEGACY)
@@ -13,6 +29,6 @@ describe('appDataHexToCid', () => {
     // when
     const promise = appDataHexToCidLegacy('invalidHash')
     // then
-    await expect(promise).rejects.toThrow('Incorrect length')
+    await expect(promise).rejects.toThrow()
   })
 })
