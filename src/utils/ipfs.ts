685259d8b1fe812603cd3bd5cba9a0060e80c467
shoom3301 <shoom3301@gmail.com>
2025-06-24 13:25:39 +0500

    fix: do not use Buffer from global

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -1,4 +1,5 @@
 import type { CID, MultibaseDecoder } from 'multiformats/cid'
+import { Buffer } from 'buffer'
 
 // CID uses multibase to self-describe the encoding used (See https://github.com/multiformats/multibase)
 //   - Most reference implementations (multiformats/cid or Pinata, etc) use base58btc encoding
